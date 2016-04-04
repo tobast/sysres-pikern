45f83fb009f4bcd77763c296384e86af24e8894b
Ekdohibs <nathanael.courant@laposte.net>
2016-04-04 23:45:07 +0200

    Blink LED

--- kernel/source/main.s
+++ kernel/source/main.s
@@ -6,8 +6,27 @@ _start:
 	mov r1,#1
 	lsl r1,#18
 	str r1,[r0,#4]
-	mov r1,#0
+
+loop$:
+
+	mov r2,#0x3F0000
+wait1$:
+	sub r2,#1
+	cmp r2,#0
+	bne wait1$
+
+	mov r1,#1
 	lsl r1,#16
 	str r1,[r0,#40]
-loop$:
+
+	mov r2,#0x3F0000
+wait2$:
+	sub r2,#1
+	cmp r2,#0
+	bne wait2$
+
+	mov r1,#1
+	lsl r1,#16
+	str r1,[r0,#28]
+	
 	b loop$
