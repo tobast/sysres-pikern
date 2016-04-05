d0b5ae107873619947c16715699a2aa0f8ff5530
Théophile Bastian <contact@tobast.fr>
2016-04-06 01:15:24 +0200

    Documented code.

--- network/layers/Ipv4Decorator.h
+++ network/layers/Ipv4Decorator.h
@@ -13,17 +13,28 @@ class Ipv4Decorator {
 		/// Thrown when trying to extract a packet that is not complete yet.
 
 		Ipv4Decorator(Ipv4Addr sourceIp);
+		/// Creates a decorator using the given IP as source of the packets.
+
 		Bytes decorate(const Bytes& data, Ipv4Addr destIp);
+		/// Decorates the given [data] with an IPv4 header, ready to be sent
+		/// to [destIp].
+	
 		Bytes extract(const Bytes& data);
+		/// Extracts the data from the IPv4 decorated packet [data]. If the
+		/// packet is fragmented and is not fully received yet, this function
+		/// throws [IncompletePacket].
 	
 	private:
 		typedef char16_t Uuid;
 		typedef uint8_t uchar;
 
 		std::unordered_map<Uuid,Bytes> uncompleted;
+		/// Keeps the packets that are not yet fully received.
 		Uuid uuid;
+		/// Current packet UUID, incremented each time.
 
 		Ipv4Addr sourceIp;
+		/// Address from which we are sending packets.
 };
 
 #endif//DEF_NW_IPV4
