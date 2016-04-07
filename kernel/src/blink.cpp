5c7e045f61d3d134f7437e7dbe9f92e4d46d1b12
Ekdohibs <nathanael.courant@laposte.net>
2016-04-07 14:37:25 +0200

    Interrupts -- part 1.

--- kernel/src/blink.cpp
+++ kernel/src/blink.cpp
@@ -6,6 +6,48 @@ typedef int32_t Int;
 const char GPIO_WAY_INPUT =		0b000;
 const char GPIO_WAY_OUTPUT =	0b001;
 
+typedef void (*interrupt)(Int*);
+extern interrupt _interrupt_handler_addr;
+interrupt volatile * const interrupt_handler_addr = &_interrupt_handler_addr; 
+extern interrupt _irq_handler_addr;
+interrupt volatile * const irq_handler_addr = &_irq_handler_addr;
+extern Int _vector_table;
+Int volatile * const vector_table = &_vector_table;
+Int volatile * const vector_table_dst = 0;
+const Int vector_table_length = 15;
+
+/***************************************
+ *************** WARNING ***************
+ ***************************************
+ * If you modify this, you MUST modify *
+ * vector_table_length above to match  *
+ * the actual length of the table.     *
+ ****************************************/
+
+asm(
+".text\n\t"
+"_vector_table:\n\t"
+	"ldr pc, _interrupt_handler_addr\n\t" // reset
+	"ldr pc, _interrupt_handler_addr\n\t" // undefined
+	"ldr pc, _interrupt_handler_addr\n\t" // svc
+	"ldr pc, _interrupt_handler_addr\n\t" // prefetch
+	"ldr pc, _interrupt_handler_addr\n\t" // abort
+	"ldr pc, _interrupt_handler_addr\n\t" // reserved
+	//"ldr pc, _interrupt_handler_addr\n\t" // irq
+	"b _asm_irq_handler\n\t" // irq
+	"ldr pc, _interrupt_handler_addr\n\t" // fiq
+"_interrupt_handler_addr:\n\t"
+	".space 4\n\t"
+"_irq_handler_addr:\n\t"
+	".space 4\n\t"
+"_asm_irq_handler:\n\t"
+	"push {r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,lr}\n\t" // Save registers
+	"ldr r0, _irq_handler_addr\n\t"
+    "blx r0\n\t"
+    "pop  {r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,lr}\n\t" // Restore them
+    "subs pc,lr,#4\n\t"
+);
+
 inline void gpioSet(Int i) {
 	// Sets the bit i of GPIO + 0x1C
 	Int orMask = (1<<(i%32));
@@ -29,9 +71,25 @@ void sleep_us(Int us) {
 	while ((uint32_t)(TIMER[1] - init_count) < (uint32_t) us) {};
 }
 
+void init_vector_table() {
+	for (Int i = 0; i < vector_table_length; i++) {
+		vector_table_dst[i] = vector_table[i];
+	}
+}
+
+void set_irq_handler(interrupt handler) {
+	*((interrupt*)((Int)vector_table_dst + (Int)irq_handler_addr - (Int)vector_table)) = handler;
+}
+
+void on_interrupt(Int* args) {
+	
+}
+
 __attribute__((naked))
 __attribute__((section(".init")))
 int main(void) {
+	init_vector_table();
+	set_irq_handler(&on_interrupt);
 	const Int LED_GPIO = 25, ACT_GPIO = 16;
 	gpioSetWay(LED_GPIO, GPIO_WAY_OUTPUT);
 	gpioSetWay(ACT_GPIO, GPIO_WAY_OUTPUT);
