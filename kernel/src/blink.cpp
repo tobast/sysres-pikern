0ae21f9c8fa5d410ce33e8acddbdbab4d1f785df
Ekdohibs <nathanael.courant@laposte.net>
2016-04-12 11:38:01 +0200

    Async code finally working

--- kernel/src/blink.cpp
+++ kernel/src/blink.cpp
@@ -10,15 +10,17 @@ typedef int32_t Int;
 const char GPIO_WAY_INPUT =		0b000;
 const char GPIO_WAY_OUTPUT =	0b001;
 
-typedef void (*interrupt)(Int*);
-extern interrupt _interrupt_handler_addr;
-interrupt volatile * const interrupt_handler_addr = &_interrupt_handler_addr; 
-extern interrupt _irq_handler_addr;
-interrupt volatile * const irq_handler_addr = &_irq_handler_addr;
+typedef void (*interrupt_handler)(Int*);
+extern interrupt_handler _interrupt_handler_addr;
+interrupt_handler volatile * const interrupt_handler_addr = &_interrupt_handler_addr; 
+//extern interrupt_handler _irq_handler_addr;
+//interrupt_handler volatile * const irq_handler_addr = &_irq_handler_addr;
+extern interrupt_handler _asm_irq_handler_addr;
+interrupt_handler volatile * const asm_irq_handler_addr = &_asm_irq_handler_addr;
 extern Int _vector_table;
 Int volatile * const vector_table = &_vector_table;
 Int volatile * const vector_table_dst = 0;
-const Int vector_table_length = 15;
+const Int vector_table_length = 10;
 
 /***************************************
  *************** WARNING ***************
@@ -38,19 +40,100 @@ asm(
 	"ldr pc, _interrupt_handler_addr\n\t" // abort
 	"ldr pc, _interrupt_handler_addr\n\t" // reserved
 	//"ldr pc, _interrupt_handler_addr\n\t" // irq
-	"b _asm_irq_handler\n\t" // irq
+	//"b _asm_irq_handler\n\t" // irq
+	"ldr pc, _asm_irq_handler_addr\n\t" // irq
 	"ldr pc, _interrupt_handler_addr\n\t" // fiq
 "_interrupt_handler_addr:\n\t"
 	".space 4\n\t"
-"_irq_handler_addr:\n\t"
-	".space 4\n\t"
-"_asm_irq_handler:\n\t"
-	"push {r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,lr}\n\t" // Save registers
-	"ldr r0, _irq_handler_addr\n\t"
-    "blx r0\n\t"
-    "pop  {r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,lr}\n\t" // Restore them
-    "subs pc,lr,#4\n\t"
+"_asm_irq_handler_addr:\n\t"
+    ".space 4\n\t"
 );
+
+//"_irq_handler_addr:\n\t"
+//	".space 4\n\t"
+
+//extern interrupt_handler _irq_handler_addr;
+//interrupt_handler volatile * const irq_handler_addr = &_irq_handler_addr;
+interrupt_handler _irq_handler_addr;
+interrupt_handler volatile * const irq_handler_addr = &_irq_handler_addr;
+
+__attribute__((naked))
+void _asm_irq_handler(Int*) {
+	asm(
+		"stmfd sp, {r0-lr}^\n\t" // Save registers
+		"sub sp, sp, #60\n\t" // We saved 15 registers
+		"mrs r0, spsr\n\t" // Get SPSR
+		"stmfd sp!, {r0, lr}\n\t" // Save it and lr as well
+		"ldr r1, =_irq_handler_addr\n\t"
+		"ldr r1, [r1]\n\t"
+		"mov r0, sp\n\t" // Save stack pointer as first argument
+		"blx r1\n\t"
+		"ldmfd sp!, {r0, lr}\n\t" // Restore link register and value of spsr
+		"msr spsr_cxsf, r0\n\t"
+		"ldmfd sp, {r0-lr}^\n\t" // Restore them
+		"add sp, sp, #60\n\t"
+		"nop\n\t" // Needed?
+		"subs pc, lr, #4" // And... jump.
+	);
+}
+
+struct context {
+	Int spsr;
+	Int lr;
+	Int r0;
+	Int r1;
+	Int r2;
+	Int r3;
+	Int r4;
+	Int r5;
+	Int r6;
+	Int r7;
+	Int r8;
+	Int r9;
+	Int r10;
+	Int r11;
+	Int r12;
+	Int r13;
+	Int r14;
+	context () {
+		r0 = r1 = r2 = r3 = r4 = r5 = r6 = r7 = 0;
+		r8 = r9 = r10 = r11 = r12 = r13 = r14 = 0;
+		spsr = lr = 0;
+	};
+};
+Int volatile active = 0;
+context stored_state[2];
+
+void on_irq(Int* stack_pointer) {
+	context* current_context = (context*)stack_pointer;
+	stored_state[active] = *current_context;
+	active = 1 - active;
+	*current_context = stored_state[active];
+	ARM_TIMER[3] = 0;
+	//ARM_TIMER[0] = 500000;
+	ARM_TIMER[0] = 500;
+}
+
+typedef void (*async_func)();
+void async_start(int i, async_func f) {
+	stored_state[i] = context();
+	stored_state[i].lr = ((Int)f) + 4;
+	// Find a suitable stack pointer
+	stored_state[i].r13 = 0x100000 + 0x80000 * (i + 1);
+	stored_state[i].spsr = 0x53; // User mode, enable IRQ, disable FIQ // SVC mode actually
+}
+
+void async_go() {
+	asm(
+		"ldr sp, =stored_state\n\t"
+		"ldmfd sp!, {r0, lr}\n\t"
+		"msr spsr_cxsf, r0\n\t"
+		"ldmfd sp, {r0-lr}^\n\t"
+		"add sp, sp, #60\n\t"
+		"nop\n\t" // Needed?
+		"subs pc, lr, #4"
+	);
+}
 
 void init_stacks() {
 	asm(
@@ -126,10 +209,12 @@ void init_vector_table() {
 	for (Int i = 0; i < vector_table_length; i++) {
 		vector_table_dst[i] = vector_table[i];
 	}
+	*((interrupt_handler*)((Int)vector_table_dst + (Int)asm_irq_handler_addr - (Int)vector_table)) = &_asm_irq_handler;
 }
 
-void set_irq_handler(interrupt handler) {
-	*((interrupt*)((Int)vector_table_dst + (Int)irq_handler_addr - (Int)vector_table)) = handler;
+void set_irq_handler(interrupt_handler handler) {
+	//*((interrupt_handler*)((Int)vector_table_dst + (Int)irq_handler_addr - (Int)vector_table)) = handler;
+	*irq_handler_addr = handler;
 }
 
 Int volatile count = 0;
@@ -137,9 +222,8 @@ Int volatile count = 0;
 const Int LED_GPIO = 25;
 const Int ACT_GPIO = 16;
 
-void on_interrupt(Int* args) {
+void on_irq_blk(Int* stack_pointer) {
 	count++;
-	//gpioSet(ACT_GPIO);
 	if (count & 1) {
 		gpioSet(LED_GPIO);
 	} else {
@@ -149,7 +233,26 @@ void on_interrupt(Int* args) {
 	//TIMER[3] = TIMER[1] + 500000;
 
 	ARM_TIMER[3] = 0;
-	ARM_TIMER[0] = 500000 - 20000 * (count & 15);
+	//ARM_TIMER[0] = 500000 - 20000 * (count & 15);
+	ARM_TIMER[0] = 500000;
+}
+
+void led_blink() {
+	while(1) {
+		gpioSet(LED_GPIO);
+		sleep_us(2*500000);
+		gpioUnset(LED_GPIO);
+		sleep_us(500000);
+	}
+}
+
+void act_blink() {
+	while(1) {
+		gpioSet(ACT_GPIO);
+		sleep_us(2*300000);
+		gpioUnset(ACT_GPIO);
+		sleep_us(300000);
+	}
 }
 
 __attribute__((naked))
@@ -157,8 +260,7 @@ __attribute__((section(".init")))
 int main(void) {
 	init_vector_table();
 	init_stacks();
-	set_irq_handler(&on_interrupt);
-	enable_irq();
+	set_irq_handler(&on_irq);
 	gpioSetWay(LED_GPIO, GPIO_WAY_OUTPUT);
 	gpioSetWay(ACT_GPIO, GPIO_WAY_OUTPUT);
 
@@ -169,8 +271,8 @@ int main(void) {
 	ARM_TIMER[3] = 0;
 	ARM_TIMER[7] = 0xff;
 	ARM_TIMER[2] = 0x3e00a2;
-	ARM_TIMER[0] = 500000;
-	ARM_TIMER[6] = 500000;
+	//ARM_TIMER[0] = 500000;
+	//ARM_TIMER[6] = 500000;
 
 	//TIMER[3] = TIMER[1] + 500000;
 
@@ -182,6 +284,14 @@ int main(void) {
 		gpioUnset(ACT_GPIO);
 		sleep_us(500000);
 	}*/
-	while (1) {}
+	ARM_TIMER[0] = 500;
+	ARM_TIMER[6] = 500;
+
+	async_start(0, &led_blink);
+	async_start(1, &act_blink);
+	enable_irq();
+	async_go();
+	//act_blink();
+	//while (1) {}
 }
 
