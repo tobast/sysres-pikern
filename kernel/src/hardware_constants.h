bd9ceb627a1e6a6d3b3647757f1ecdc6d4109ea4
Théophile Bastian <contact@tobast.fr>
2016-04-16 13:05:39 +0200

    Added mailbox memory locations

--- kernel/src/hardware_constants.h
+++ kernel/src/hardware_constants.h
@@ -7,6 +7,13 @@
 #include "common.h"
 
 namespace hardware {
+	namespace mailbox {
+		// Mailbox protocol
+		static volatile u32 *READ = (u32*) 0x2000b880;
+		static volatile u32 *STATUS = (u32*) 0x2000b898;
+		static volatile u32 *WRITE = (u32*) 0x2000b8a0;
+	};
+
 #ifdef HW_PI_1B
 	s32 volatile * const GPIO = (s32 volatile *)0x20200000;
 
