04797453331bff52d403748a675ebc7fac6c04e0
Théophile Bastian <contact@tobast.fr>
2016-04-18 23:36:40 +0200

    Now supporting a whole byte of LEDs

--- kernel/src/gpio.cpp
+++ kernel/src/gpio.cpp
@@ -4,8 +4,14 @@ namespace gpio {
 	void init() {
 		gpio::setWay(gpio::ACT_PIN, gpio::WAY_OUTPUT);
 		gpio::setWay(gpio::CRASH_PIN, gpio::WAY_OUTPUT);
+		gpio::setWay(gpio::LED_PIN, gpio::WAY_OUTPUT);
+		for(int i=0; i < 8; i++) {
+			setWay(BYTE_PINS[i], gpio::WAY_OUTPUT);
+			unset(BYTE_PINS[i]);
+		}
 
 		gpio::set(gpio::ACT_PIN);
 		gpio::unset(gpio::CRASH_PIN);
+		gpio::unset(gpio::LED_PIN);
 	}
 }
