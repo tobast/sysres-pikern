05cea0e95376f13ec219240553751bf33e89359c
Théophile Bastian <contact@tobast.fr>
2016-04-22 10:16:28 +0200

    FIX malloc - brk not initialized + wrong assert

--- kernel/src/malloc.cpp
+++ kernel/src/malloc.cpp
@@ -1,15 +1,21 @@
 #include "malloc.h"
 
-const size_t MEMORY_START = 0x2000000, MEMORY_END = 0x10000000;
+#include "gpio.h"
 
-void* malloc(unsigned size) {
+const uint32_t MEMORY_START(0x2000000), MEMORY_END(0x10000000);
+uint32_t brk(MEMORY_START);
+
+void mallocInit() {
+	brk = MEMORY_START;
+}
+
+void* malloc(uint32_t size) {
 	// For now, this is just a SBRK function.
-	static size_t brk = MEMORY_START;
-	assert(brk + size >= MEMORY_END);
+	assert(brk + size <= MEMORY_END);
 	
 	void* out = (void*)brk;
 	brk += size;
-	brk += -brk & 3; // Aligns to 4-bytes.
+	brk += (-brk) & 3; // Aligns to 4-bytes.
 	return out;
 }
 
--- kernel/src/malloc.h
+++ kernel/src/malloc.h
@@ -1,11 +1,13 @@
 #ifndef DEF_KER_MALLOC
 #define DEF_KER_MALLOC
 
-#include <cstdlib>
+#include <cstdint>
 
 #include "assert.h"
 
-void* malloc(unsigned size);
+void mallocInit();
+
+void* malloc(uint32_t size);
 /// Allocates a block of [size] bytes in memory, returns a pointer to the first
 /// byte of the allocated block.
 /// Crashes the system whenever the memory limit is reached.
