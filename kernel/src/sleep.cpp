88f7834494174e5e022d05367288eb8169bfa027
Théophile Bastian <contact@tobast.fr>
2016-04-22 14:44:45 +0200

    Added ellapsed_us function, based on sleep()'s code.

--- kernel/src/sleep.cpp
+++ kernel/src/sleep.cpp
@@ -1,5 +1,5 @@
 #include "sleep.h"
-#include "common.h"
+//#include "common.h" // Now included in header
 #include "hardware_constants.h"
 
 void sleep_us(int us) {
@@ -7,5 +7,10 @@ void sleep_us(int us) {
 	 * NOTE: May not work if us is too close to 0 or max_Int (32b) */
 	u32 init_count = hardware::GPIO_TIMER[1];
 	while ((u32)(hardware::GPIO_TIMER[1] - init_count) < (u32) us) {};
+}
+
+u32 ellapsed_us() {
+	return hardware::GPIO_TIMER[1];
+	// Copied from above. @Ekdohibs: TODO review
 }
 
--- kernel/src/sleep.h
+++ kernel/src/sleep.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "common.h"
+
 void sleep_us(int us);
 /// Sleeps for [us] microseconds.
+
+u32 ellapsed_us();
+/// Returns the number of ellapsed microseconds since powered up.
 
