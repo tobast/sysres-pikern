8076272066baeb5aeb7d278a3bf202c3f0773012
Théophile Bastian <contact@tobast.fr>
2016-04-22 16:28:35 +0200

    Started interfacing with uspi

---
+++ kernel/src/uspi_interface.cpp
@@ -0,0 +1,51 @@
+#include "uspi_interface.h"
+
+#include "common.h"
+#include "sleep.h"
+#include "mailbox.h"
+
+void usDelay(unsigned nMicroSeconds) {
+	sleep_us(nMicroSeconds);
+}
+void MsDelay(unsigned nMilliSeconds) {
+	sleep_us(1000*nMilliSeconds);
+}
+
+/// Powers on the given device, and wait until completed.
+int SetPowerStateOn(unsigned nDeviceId) {
+	uint32_t stabTime = mailbox::getPowerupTiming(nDeviceId);
+	if(stabTime == 0) { // Device does not exists
+		return 0;
+	}
+	
+	uint32_t curState = mailbox::getPowerState(nDeviceId);
+	if(curState & 0x01) { // Already powered up
+		return 1;
+	}
+
+	uint32_t nState = mailbox::setPowerState(nDeviceId, 0x11);
+	if(nState & 0x01 == 0) { // Not powered up
+		return 0;
+	}
+	sleep_us(3*stabTime);
+}
+
+/// Returns the MAC address of the device.
+int GetMacAddress(u8 Buffer[6]) {
+	uint64_t mac = mailbox::getMac();
+	for(int i=0; i < 6; i++) {
+		// FIXME: right byte order?
+		Buffer[i] = (u8) mac;
+		mac >>= 8;
+	}
+	return 1; // success
+}
+
+void LogWrite(const char* /*pSource*/,
+		unsigned /*Severity*/,
+		const char* /*pMessage*/,
+		...)
+{
+	// TODO when we'll be able to dump it somewhere.
+}
+
---
+++ kernel/src/uspi_interface.h
@@ -0,0 +1,10 @@
+/****
+ * Interface for uspi. Implements all the functions needed by uspios.h,
+ * and might implement more in the future.
+ ****/
+
+#pragma once
+
+// Implements at least the requirements of uspios.h
+#include "uspios.h"
+
