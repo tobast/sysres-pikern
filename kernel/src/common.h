030e8667d26f6c0de31c23752f4f11c16d59d9ed
Théophile Bastian <contact@tobast.fr>
2016-04-22 16:14:21 +0200

    Added (s|u)(8|16) types in common.h

--- kernel/src/common.h
+++ kernel/src/common.h
@@ -5,6 +5,10 @@
 
 typedef int32_t s32;
 typedef uint32_t u32;
+typedef int16_t s16;
+typedef uint16_t u16;
+typedef int8_t s8;
+typedef uint8_t u8;
 
 inline u32 min(u32 a, u32 b) {
 	return (a < b) ? a : b;
