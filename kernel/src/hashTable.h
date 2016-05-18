6e3adec213e43a02aedfb2b865876f7aeda65553
Théophile Bastian <contact@tobast.fr>
2016-05-18 19:46:21 +0200

    Added a hashtable.

---
+++ kernel/src/hashTable.h
@@ -0,0 +1,95 @@
+#pragma once
+
+/**
+ * Custom hash table implementation.
+ **/
+
+#include <cstdint>
+#include "expArray.h"
+#include "pair.h"
+
+template <typename T> class StdToInt {
+	public:
+	uint32_t operator()(const T& v) const {
+		return (uint32_t)v;
+	}
+};
+
+template <typename Key, typename Value, class ToInt = StdToInt<Key> >
+class HashTable {
+public:
+	class NotFound {};
+
+	HashTable() : nbBuckets(7) {
+		init();
+	}
+	HashTable(unsigned nbBuckets) : nbBuckets(nbBuckets) {
+		init();
+	}
+	~HashTable() {
+		for(unsigned buck=0; buck < nbBuckets; buck++)
+			buckets[buck].~Bucket();
+		free(buckets);
+	}
+
+	void insert(const Key& k, const Value& v) {
+		unsigned bucket = hash(k);
+
+		for(unsigned pos=0; pos < buckets[bucket].size(); pos++) {
+			// If k is already bound, replace the binding
+			if(buckets[bucket][pos].first == k) {
+				buckets[bucket][pos].second = v;
+				return;
+			}
+		}
+		
+		// Else, add it, potentially triggering a rehash
+		Assoc assoc = Pair<Key,Value>(k,v);
+		buckets[bucket].push_back(assoc);
+		nbAssoc++;
+		if(nbAssoc > nbBuckets*.8)
+			rehash(nbBuckets * 2);
+	}
+
+	const Value& find(const Key& k) const {
+		unsigned buck = hash(k);
+		for(unsigned pos=0; pos < buckets[buck].size(); pos++) {
+			if(buckets[buck][pos].first == k)
+				return buckets[buck][pos].second;
+		}
+		throw NotFound();
+	};
+
+
+private: //meth
+	void init() {
+		nbAssoc=0;
+		buckets = (Bucket*) malloc(nbBuckets*sizeof(Bucket));
+	}
+	unsigned hash(const Key& k) const {
+		return (ToInt()(k)) % nbBuckets;
+	}
+
+	void rehash(unsigned nNbBuckets) {
+		Bucket* nBuckets = (Bucket*) malloc(nNbBuckets * sizeof(Bucket));
+		unsigned prevBuckets = nbBuckets;
+		nbBuckets = nNbBuckets;
+
+		for(unsigned buck = 0; buck < prevBuckets; buck++) {
+			for(unsigned asso = 0; asso < buckets[buck].size(); asso++) {
+				Assoc& assoc = buckets[buck][asso];
+				nBuckets[hash(assoc.first)].push_back(assoc);
+			}
+			buckets[buck].~Bucket();
+		}
+		free(buckets);
+		buckets = nBuckets;
+	}
+
+private:
+	typedef Pair<Key,Value> Assoc;
+	typedef ExpArray<Assoc> Bucket;
+	Bucket* buckets;
+	unsigned nbBuckets, nbAssoc;
+};
+
