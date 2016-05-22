a5b2dad9049b3d260d00f633b963af73e309c086
Théophile Bastian <contact@tobast.fr>
2016-05-22 19:00:25 +0200

    Colored bar between each connection to easily see that a new one was established

--- tools/udpListen.py
+++ tools/udpListen.py
@@ -13,6 +13,19 @@ if len(sys.argv) > 1:
     else:
         port = int(argv[1])
 
+nextBgColor = 0
+def nextColorBar():
+    global nextBgColor
+    COLOR_HEAD = '\033['
+    COLOR_BG_START = 40
+    COLOR_RESET = COLOR_HEAD+'39;49m'
+    BG_COLORS = [ COLOR_HEAD+str(COLOR_BG_START+i)+'m' for i in range(1,8) ]
+
+    print(BG_COLORS[nextBgColor]+\
+            "                                                             "+\
+            COLOR_RESET)
+    nextBgColor = (nextBgColor+1)%len(BG_COLORS)
+
 class LogDump(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -26,6 +39,7 @@ class LogDump(threading.Thread):
             if(data == b'=PIKERN Hey, listen!='):
                 sock.sendto(b"RPi", addr)
                 time=datetime.datetime.now().strftime('%H:%M:%S')
+                nextColorBar()
                 print("[{}] Invited to {}".format(time,addr[0]))
                 continue
 
