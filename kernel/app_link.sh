95f41f246f5608755d2bbfd98d642064cf3b6a2e
Ekdohibs <nathanael.courant@laposte.net>
2016-05-24 17:56:16 +0200

    Fix app_link.sh

--- kernel/app_link.sh
+++ kernel/app_link.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+tempfile=$(mktemp)
+arm-none-eabi-objcopy $1 -O binary $tempfile
 arm-none-eabi-objdump -t $1 |
 	grep __bss_end__ |
 	cut -d' ' -f1 |
-	python3 -c "import sys; x = int(input(), 16); sys.stdout.buffer.write(bytes((0x7f, 0x45, 0x4c, 0x46, (x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff)))"
-arm-none-eabi-objcopy $1 -O binary /dev/stdout
+	python3 -c "import sys; x = int(input(), 16); sys.stdout.buffer.write(bytes((0x7f, 0x45, 0x4c, 0x46, (x >> 24) & 0xff, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff)))" |
+	cat - $tempfile > $2
+rm $tempfile
 
