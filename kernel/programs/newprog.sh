0774aba3d3e26a2029173b0d04e5a866564b1bc1
Ekdohibs <nathanael.courant@laposte.net>
2016-05-25 20:02:20 +0200

    Fix new code template.

--- kernel/programs/newprog.sh
+++ kernel/programs/newprog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -r __template "$1"
-echo -e "#inlcude \"syslib.h\"\n\nint main(int argc, char** argv) {\n}\n" > "$1/$1.cpp"
+echo -e "#include \"syslib.h\"\n\nint main(int argc, char** argv) {\n}\n" > "$1/$1.cpp"
