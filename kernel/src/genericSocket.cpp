6dbbc8c7478a6ba8a909311f49f1e28a6cf7f422
Théophile Bastian <contact@tobast.fr>
2016-05-25 17:24:04 +0200

    Generic socket quite stable

---
+++ kernel/src/genericSocket.cpp
@@ -0,0 +1,73 @@
+#include "genericSocket.h"
+#include "assert.h"
+
+GenericSocket::GenericSocket(bool blockingState, unsigned maxSize) :
+	blocking(blockingState), maxSize(maxSize), sinceLastDelim(0)
+{}
+
+unsigned GenericSocket::write(const void* inData, unsigned len, bool addDelim)
+{
+	if(len == 0)
+		return 0;
+	unsigned actualLen = min(len, maxSize - data.size());
+
+	// Force at least one loop turn, to run writeByte's checks
+	for(unsigned pos=0; pos < max(1u, actualLen); pos++)
+		writeByte(((uint8_t*)inData)[pos]);
+	if(addDelim && actualLen == len)
+		writeDelimiter();
+	return actualLen;
+}
+void GenericSocket::writeByte(uint8_t v) {
+	if(isFull()) {
+		assert(!blocking, 0);
+		// Else, we're a non-blocking sock
+		return;
+	}
+	sinceLastDelim++;
+	data.push(v);
+}
+void GenericSocket::writeDelimiter() {
+	delims.push(sinceLastDelim);
+	sinceLastDelim = 0;
+}
+
+unsigned GenericSocket::read(void* buff, unsigned maxSize) {
+	unsigned len = min(maxSize, data.size());
+	if(!delims.empty() && delims.front() == 0)
+		delims.pop();
+
+	for(unsigned pos=0; pos < len; pos++) {
+		if(!delims.empty() && delims.front() == 0) {
+			delims.pop();
+			return pos;
+		}
+		((uint8_t*)buff)[pos] = readByte();
+	}
+
+	return len;
+}
+uint8_t GenericSocket::readByte() {
+	if(isEmpty())
+		throw Empty();
+
+	if(delims.empty())
+		sinceLastDelim--;
+	else {
+		assert(delims.front() != 0, 0);
+		delims.front()--;
+	}
+
+	return data.pop();
+}
+
+bool GenericSocket::isFull() {
+	return data.size() == maxSize;
+}
+bool GenericSocket::isEmpty() {
+	return data.size() == 0;
+}
+bool GenericSocket::isBlocking() {
+	return blocking;
+}
+
---
+++ kernel/src/genericSocket.h
@@ -0,0 +1,63 @@
+#pragma once
+
+/**
+ * Generic socket: allows reading/writing chars, placing packet delimiters, ...
+ **/
+
+#include "common.h"
+#include "queue.h"
+
+const int DFT_MAX_SIZE=2048;
+
+class GenericSocket {
+	public:
+		class Empty {}; // Raised by [readByte] on an empty queue.
+
+		GenericSocket(bool blockingState, unsigned maxSize=DFT_MAX_SIZE);
+		/**
+		 * If [blockingState] is true, trying to write to a full socket will
+		 * lead to an assert failure. *ALWAYS* check that a blocking socket
+		 * is not full before writing...
+		 * If [blockingState] is false, trying to write to a full socket will
+		 * drop the packet.
+		 **/
+
+		unsigned write(const void* inData, unsigned len, bool addDelim=false);
+		/**
+		 * Writes the first [len] bytes of [inData] to the socket. If the
+		 * socket is full, will act depending on the socket's state (see
+		 * constructor). If the socket has not enough free space to write
+		 * [len] bytes, all bytes that can be written will be.
+		 * If [addDelim] is true, [writeDelimiter()] will be called after
+		 * writing the [len] bytes.
+		 * Returns the number of bytes actually written.
+		 **/
+		void writeByte(uint8_t v);
+		/** Same as [write], but for one character only. */
+		void writeDelimiter();
+		/**
+		 * Adds a packet delimiter at the current position, which will
+		 * force reading methods to stop at this point, rather than
+		 * concatenate two unrelated packets in one call.
+		 **/
+
+		unsigned read(void* buff, unsigned maxSize);
+		/**
+		 * Tries to read at most [maxSize] bytes to [buff]. Stops on an
+		 * empty socket, or when a delimiter is encountered.
+		 **/
+		uint8_t readByte();
+		/** Reads a single byte from the socket. Raises [Empty] if empty.
+		 * Fails on assert if there is a delimiter at front.
+		 **/
+
+		bool isFull();
+		bool isEmpty();
+		bool isBlocking();
+
+		Queue<uint8_t> data, delims;
+	private:
+		bool blocking;
+		unsigned maxSize, sinceLastDelim;
+};
+
