33510d382c2f7da54974ac2e3da573568904abfd
Théophile Bastian <contact@tobast.fr>
2016-05-28 22:59:17 +0200

    Executables now rendered green

--- kernel/programs/ls/ls.cpp
+++ kernel/programs/ls/ls.cpp
@@ -1,6 +1,14 @@
 #include "syslib.h"
 #include "proglib.h"
 
+bool isBinary(int handle) {
+	char buff[4];
+	if(file_read(handle, 0, (void*)buff, 4) != 4)
+		return false;
+	return buff[0] == 0x7f && buff[1]=='E' &&
+		buff[2]=='L' && buff[3]=='F';
+}
+
 void dispLsEntry(int handle) {
 	char name[256];
 	get_node_name(handle, name, 256);
@@ -8,6 +16,10 @@ void dispLsEntry(int handle) {
 	if(is_folder(handle))
 		printf("%F%F%s%F", AnsiFormat(ANSI_BOLD),
 				AnsiFormat(ANSI_FG, ANSI_BLUE),
+				name, AnsiFormat());
+	else if(isBinary(handle))
+		printf("%F%F%s%F", AnsiFormat(ANSI_BOLD),
+				AnsiFormat(ANSI_FG, ANSI_GREEN),
 				name, AnsiFormat());
 	else
 		printf("%s", name);
